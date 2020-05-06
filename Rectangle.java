9739535534be082a042bd3deafd5dd38b2de0e3b
Alexandra <willia@usi.ch>
2020-05-06 15:27:50 +0200

    Added rectangle class

---
+++ Rectangle.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Rectangle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Rectangle
+{
+    // instance variables - replace the example below with your own
+    private Coordinate topLeft;
+    private Coordinate bottomRight;
+
+    /**
+     * Constructor for objects of class Rectangle
+     */
+    public Rectangle()
+    {
+  
+    }
+
+}
