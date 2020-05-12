346c0171e5f880dd4560fed0dfd3cf3ac697407e
John Tomazoni <joao.tomazoni@usi.ch>
2020-05-12 02:08:36 +0200

    Low effort TUI for testing

--- Main.java
+++ Main.java
@@ -1,20 +1,45 @@
-/**
- * ...
- */
+///**
+// * ...
+// */
 public class Main {
-    
+
     private Main() {
         // never instantiated
     }
-    
+
     /**
      * Run the application (from the command line).
-     * @param args the command line arguments
+     * @param args the command line arguments.
      */
     public static void main(final String[] args) {
-        final Model model = new Model();
-        final TextUserInterface tui = new TextUserInterface(model);
-        tui.run();
+        TextUserInterface input = new TextUserInterface();
+        System.out.print("Type one of the available options: \"start\", \"help\" or \"quit\" ");
+        boolean b = true;
+        while (b) {
+            System.out.print("\n> ");
+            String words = input.getCommand().nextLine();
+            // Switch is bad coding, I will put it into TUI Class eventually.
+            if (input.checkCommand(words)) {
+                switch (words) {
+                    case "start":
+                        input.printStart();
+                        b = false;
+                        break;
+                    case "help":
+                        input.printHelp();
+                        b = false;
+                        break;
+                    case "quit":
+                        input.printQuit();
+                        b = false;
+                        break;
+                }
+            } else {
+                System.out.println(input.printDefault() + "\"" + words + "\"");
+            }
+        }
     }
-    
 }
+//        final Model model = new Model();
+//        final TextUserInterface tui = new TextUserInterface(model);
+//        tui.run();
--- TextUserInterface.java
+++ TextUserInterface.java
@@ -1,24 +1,76 @@
+import java.util.Scanner;
+
 /**
- * ...
+ * Provides de TUI for the game.
  */
 public class TextUserInterface {
+    private Scanner command = new Scanner(System.in);
 
-    private final Model model;
-    
-    
     /**
-     * Create a new TUI operating on the given Model.
-     * @param model The model of this application
+     * Default command getter.
+     * @return command.
      */
-    public TextUserInterface(final Model model) {
-        this.model = model;
+    public Scanner getCommand() {
+        return command;
     }
-    
+
     /**
-     * Run the application.
+     * From the course textbook Chapter 8.
+     * Available user commands.
      */
-    public void run() {
-        System.out.println("Welcome to " + model.toString());
+    private static final String[] validCommands = {
+            "start", "help", "quit"
+    };
+
+    /**
+     * From the course textbook Chapter 8.
+     * Iterates through a list of possible valid commands.
+     * @return True if a valid command has been found, false otherwise.
+     */
+    public boolean checkCommand(String str) {
+        for (String s: validCommands) {
+            if (s.equals(str)) {
+                return true;
+            }
+        }
+        return false;
     }
-    
+
+    /**
+     * Prints a message for now, will start the game later.
+     */
+    public void printStart()
+    {
+        System.out.println("New Game Starts");
+        //TODO
+        // we need to find a way to print the elapsed time every time
+        // a Tick() happens
+    }
+
+    /**
+     * Display available commands.
+     */
+    public void printHelp()
+    {
+        System.out.println("Use the arrow keys to move around the board");
+        System.out.println("Each player has 3 lives, game ends when one reaches 0 lives");
+        System.out.println("Start command starts a new game");
+        System.out.println("Quit command terminates this program");
+    }
+
+    /**
+     * Politely terminates the program.
+     */
+    public void printQuit()
+    {
+        System.out.println("GoodBye");
+    }
+
+    /**
+     * Default String for miss inputs.
+     * @return an error String.
+     */
+    public String printDefault() {
+        return "The commands are: \"start\", \"help\", \"quit\". " + "You typed: ";
+    }
 }
