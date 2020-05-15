756bfd957d83a1f86ff42b9bc2f5c43f545e58c2
John Tomazoni <joao.tomazoni@usi.ch>
2020-05-15 21:38:03 +0200

    Better error handling, better colision

--- Main.java
+++ Main.java
@@ -33,56 +33,51 @@ public class Main {
                             System.out.print("\n> ");
                             String input1 = nInput.getCommand().nextLine();
                             // put all inputs from System.in into an array, every time you read a " " you add the following input as new command.
-                            // TODO make it only accept commands.length == 2
                             String[] commands = input1.split(" ");
-                            // print what was given as command for player 1 and for player 2, respectively.
-                            System.out.println("p1: " + commands[0] + " " + "p2: " + commands[1]);
-                            // check is the commands are valid commands on the list of player commands inside class TextUserInterFace.
-                            if (nInput.checkPlayerCommand(commands[0]) && nInput.checkPlayerCommand(commands[1])) {
-                                // p1 takes first argument and move accordingly.
-                                p1.act(commands[0]);
-                                // p2 takes second argument and move accordingly.
-                                p2.act(commands[1]);
-                                // print both players coordinates after the movement has happened.
-                                System.out.println("p1: (" + p1.getSnake().getHead().getX() + ", " + p1.getSnake().getHead().getY()
-                                        + ") " + "p2: (" + p2.getSnake().getHead().getX() + ", " + p2.getSnake().getHead().getY() + ")");
+                            if (commands.length == 2) {
+                                // print what was given as command for player 1 and for player 2, respectively.
+                                System.out.println("p1: " + commands[0] + " " + "p2: " + commands[1]);
+                                // check is the commands are valid commands on the list of player commands inside class TextUserInterFace.
+                                if (nInput.checkPlayerCommand(commands[0]) && nInput.checkPlayerCommand(commands[1])) {
+                                    // p1 takes first argument and move accordingly.
+                                    p1.act(commands[0]);
+                                    // p2 takes second argument and move accordingly.
+                                    p2.act(commands[1]);
+                                    // print both players coordinates after the movement has happened.
+                                    System.out.println("p1: (" + p1.getSnake().getHead().getX() + ", " + p1.getSnake().getHead().getY()
+                                            + ") " + "p2: (" + p2.getSnake().getHead().getX() + ", " + p2.getSnake().getHead().getY() + ")");
 
-                                // #----- Above this is the collision part, I tried with two different approaches, Match happens to get
-                                //        a null pointer exception, I will be doing debugging to see if I understand why it's happening.
-                                //        It's important to find out to properly implement all cases of collision -----#
+                                    // #----- Collision Section -----#
 
-                                if (p1.checkCollision(p2) || p1.checkCollisionSelf()) {
-                                    p1.die();
-                                    // resetting both players position to the default position of a match
-                                    p1.createSnake(new Left(), new Coordinate(10, 10));
-                                    p2.createSnake(new Right(), new Coordinate(12, 10));
-                                    System.out.println("p1 lost this round. p1 lives = " + p1.getLives());
-                                } else if (p2.checkCollision(p1) || p2.checkCollisionSelf()) {
-                                    p2.die();
-                                    p1.createSnake(new Left(), new Coordinate(10, 10));
-                                    p2.createSnake(new Right(), new Coordinate(12, 10));
-                                    System.out.println("p2 lost this round. p2 lives = " + p2.getLives());
+                                    if (n.checkCollision() == 1) {
+                                        p1.die();
+                                        p1.createSnake(new Left(), new Coordinate(10, 10));
+                                        p2.createSnake(new Right(), new Coordinate(12, 10));
+                                        System.out.println("p1 lost this round. p1 lives = " + p1.getLives());
+                                    } else if (n.checkCollision() == 2) {
+                                        p2.die();
+                                        p1.createSnake(new Left(), new Coordinate(10, 10));
+                                        p2.createSnake(new Right(), new Coordinate(12, 10));
+                                        System.out.println("p2 lost this round. p2 lives = " + p2.getLives());
+                                    } else if (n.checkCollision() == 0) {
+                                        p1.die();
+                                        p2.die();
+                                        p1.createSnake(new Left(), new Coordinate(10, 10));
+                                        p2.createSnake(new Right(), new Coordinate(12, 10));
+                                        System.out.println("matched ended in a draw. p1 lives = " + p1.getLives() +
+                                                ", p2 lives = " + p2.getLives());
+                                    }
+
+                                    // #----- Error Handling Section -----#
+
+                                } else {
+                                    // if it's not a valid player command continue asking for inputs.
+                                    System.out.println(input.printPlayerDefault() + "\"" + input1 + "\"");
                                 }
-//                                if (n.checkCollision() == 1) {
-//                                    p1.die();
-//                                    System.out.println("p1 lost");
-//                                    b1 = false;
-//                                } else if (n.checkCollision() == 2) {
-//                                    p2.die();
-//                                    System.out.println("p2 lost");
-//                                    b1 = false;
-//                                } else if (n.checkCollision() == 0) {
-//                                    p1.die();
-//                                    p2.die();
-//                                    System.out.println("matched ended in a draw");
-//                                    b1 = false;
-//                                }
                             } else {
-                                // if it's not a valid player command continue asking for inputs.
-                                System.out.println(input.printPlayerDefault() + "\"" + commands[0] + " " + commands[1] + "\"");
+                                System.out.println(input.printPlayerDefault() + "\"" + input1 + "\"");
                             }
                         }
-                        // you reach this section when one of the players has no more lives left. Print the winner.
                         System.out.println("\nThe winner is: " + p1.victorious(p2) + "!");
                         b = false;
                         break;
